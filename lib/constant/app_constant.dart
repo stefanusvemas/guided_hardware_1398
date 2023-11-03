e062103cc4f62bd99e4fb24331b862a40bc1c49f
stefanusvemas <stefanusvemas@gmail.com>
2023-11-03 10:39:22 +0700

    first commit

---
+++ lib/constant/app_constant.dart
@@ -0,0 +1,19 @@
+class RouteConstant {
+  static const String routeToHome = "/";
+  static const String routeToQrGeneratePage = "/qr_generate";
+  static const String routeToQrScanPage = "/qr_scan";
+  static const String routeToQrCam = "/qr_cam";
+}
+
+class ButtonTextConstant {
+  static const String qrGenerate = "Generate QR Code";
+  static const String camera = "Camera";
+  static const String qrScanning = "QR scanning";
+}
+
+class labelTextConstant {
+  static const String homePageAppBarTitle = "Modul QR, Camera, Scanner";
+  static const String scanQrPlaceHolderLabel =
+      "Scan something & click to copy to clipboard!";
+  static const String txtonCopyingClipBoard = "QR code disalin ke clipboard!";
+}
---
+++ lib/views/home_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:modul_cam_qr_1398/constant/app_constant.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  navigateTo(BuildContext context, String routeName) =>
+      Navigator.pushNamed(context, routeName);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(labelTextConstant.homePageAppBarTitle),
+      ),
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            ElevatedButton(
+              onPressed: () =>
+                  navigateTo(context, RouteConstant.routeToQrGeneratePage),
+              child: const Text(ButtonTextConstant.qrGenerate),
+            ),
+            ElevatedButton(
+              onPressed: () => navigateTo(context, RouteConstant.routeToQrCam),
+              child: const Text(ButtonTextConstant.camera),
+            ),
+            ElevatedButton(
+              onPressed: () =>
+                  navigateTo(context, RouteConstant.routeToQrScanPage),
+              child: const Text(ButtonTextConstant.qrScanning),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
